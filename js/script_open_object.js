d4d1bca65f1d252f33c9834ca8c5c061daca3942
shimmaksim <shimmaksim@gmail.com>
2019-09-04 00:24:44 +0300

    betta

---
+++ js/script_open_object.js
@@ -0,0 +1,29 @@
+"use strict";
+let purpop = document.querySelector(".purpop");
+let close = document.querySelector(".button_object_cansel");
+ let object = document.querySelector(".object");
+
+purpop.addEventListener("click", () => {
+  document.getElementById(".object").style.display = "block";
+});
+
+// close.addEventListener("click", () => {
+//   overlay.classList.remove("button_object_cansel");
+// });
+
+// burger.addEventListener('click', () => {
+//     overlay.classList.add('overlay_open');
+// });
+
+// close.addEventListener('click', () => {
+//     overlay.classList.remove('overlay_open');
+// });
+
+// const button = document.querySelector(".purpop");
+// console.log(button);
+// console.log(button.textContent);
+
+// console.log(document);
+
+// const button = document.querySelectorAll(".purpop");
+// console.log(button);
